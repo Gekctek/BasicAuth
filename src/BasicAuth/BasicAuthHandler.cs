427eedbc4fe5446eea3713b46f67cee3547ae3be
Ethan Celletti <ecelletti@datstat.com>
2015-08-13 08:59:56 -0700

    Added logging to Basic auth handler

--- src/BasicAuth/BasicAuthHandler.cs
+++ src/BasicAuth/BasicAuthHandler.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Authentication;
 using Microsoft.AspNet.Authentication.Notifications;
 using Microsoft.AspNet.Http.Authentication;
 using Microsoft.AspNet.Http.Features.Authentication;
+using Microsoft.Framework.Logging;
 
 namespace edjCase.BasicAuth
 {
@@ -19,70 +20,96 @@ namespace edjCase.BasicAuth
 		/// <returns>Task that results in an authentication ticket for credential or null for unauthorized request</returns>
 		protected async override Task<AuthenticationTicket> HandleAuthenticateAsync()
 		{
-			string[] authHeaderValues;
-			bool hasAuthHeader = this.Request.Headers.TryGetValue(BasicAuthConstants.AuthHeaderName, out authHeaderValues) &&
-								 authHeaderValues.Any();
-
-			if (!hasAuthHeader)
-			{
-				return null; //TODO?
-			}
-			string basicAuthValue = authHeaderValues.First();
-			bool headerIsBasicAuth = basicAuthValue.StartsWith("Basic ");
-			if (!headerIsBasicAuth)
-			{
-				return null; //TODO?
-			}
+			this.Logger?.LogVerbose("Basic auth handling started...");
 			try
 			{
-				BasicAuthCredential credential = BasicAuthCredential.Parse(basicAuthValue);
+				string[] authHeaderValues;
+				bool hasAuthHeader = this.Request.Headers.TryGetValue(BasicAuthConstants.AuthHeaderName, out authHeaderValues) &&
+				                     authHeaderValues.Any();
 
-				AuthenticationProperties authProperties = new AuthenticationProperties();
-
-				BasicAuthInfo authInfo = new BasicAuthInfo(credential, authProperties, this.Options);
-
-				if(this.Options.AuthenticateCredential == null)
+				if (!hasAuthHeader)
 				{
-					throw new BasicAuthConfigurationException("AuthenticateCredential method was not set in the configuration");
+					this.Logger?.LogVerbose("Request does not contain a Basic auth header. Skipping.");
+					return null; //TODO?
 				}
+				string basicAuthValue = authHeaderValues.First();
+				bool headerIsBasicAuth = basicAuthValue.StartsWith("Basic ");
+				if (!headerIsBasicAuth)
+				{
+					this.Logger?.LogVerbose("Request has an authentication header but is is not the Basic auth scheme. Skipping.");
+					return null; //TODO?
+				}
+				try
+				{
+					this.Logger?.LogInformation("Basic auth request detected. Authenticating...");
 
-				AuthenticationTicket ticket = await this.Options.AuthenticateCredential(authInfo);
-				return ticket;
+					this.Logger?.LogVerbose("Attempting to parse Basic auth credential from header.");
+					BasicAuthCredential credential = BasicAuthCredential.Parse(basicAuthValue);
+					this.Logger?.LogVerbose("Successfully.");
+
+					AuthenticationProperties authProperties = new AuthenticationProperties();
+
+					BasicAuthInfo authInfo = new BasicAuthInfo(credential, authProperties, this.Options);
+
+					if (this.Options.AuthenticateCredential == null)
+					{
+						throw new BasicAuthConfigurationException("AuthenticateCredential method was not set in the configuration");
+					}
+
+					this.Logger?.LogVerbose("Calling configured credential authentication handler.");
+					AuthenticationTicket ticket = await this.Options.AuthenticateCredential(authInfo);
+					return ticket;
+				}
+				catch (Exception ex)
+				{
+					this.Logger?.LogError("Error occurred when processing a Basic authentication request.", ex);
+
+					var failedNotification = new AuthenticationFailedNotification<string, BasicAuthOptions>(this.Context, this.Options)
+					{
+						ProtocolMessage = "", //TODO
+						Exception = ex
+					};
+
+					if (this.Options.OnException != null)
+					{
+						this.Logger?.LogVerbose("Calling configured exception handler.");
+						await this.Options.OnException(failedNotification);
+					}
+					else
+					{
+						this.Logger?.LogVerbose("No configured exception handler found.");
+					}
+
+					if (failedNotification.HandledResponse)
+					{
+						this.Logger?.LogInformation("Error was handled by configured exception handler.");
+						return failedNotification.AuthenticationTicket;
+					}
+
+					if (failedNotification.Skipped)
+					{
+						this.Logger?.LogInformation("Error resulted in skipping of Basic auth processing.");
+						return null;
+					}
+
+					this.Logger?.LogInformation("Error failed to be resolved.");
+					throw;
+				}
 			}
-			catch (Exception ex)
+			finally
 			{
-				var failedNotification = new AuthenticationFailedNotification<string, BasicAuthOptions>(this.Context, this.Options)
-				{
-					ProtocolMessage = "", //TODO
-					Exception = ex
-				};
-
-				if (this.Options.OnException != null)
-				{
-					await this.Options.OnException(failedNotification);
-				}
-
-				if (failedNotification.HandledResponse)
-				{
-					return failedNotification.AuthenticationTicket;
-				}
-
-				if (failedNotification.Skipped)
-				{
-					return null;
-				}
-
-				throw;
+				this.Logger?.LogVerbose("Basic auth handler finished.");
 			}
 		}
 
 		/// <summary>
 		/// Handles unauthorized Basic auth requests
 		/// </summary>
-		/// <param name="context"></param>
+		/// <param name="context">Context of authentication challenge</param>
 		/// <returns>True if response is handled</returns>
 		protected override Task<bool> HandleUnauthorizedAsync(ChallengeContext context)
 		{
+			this.Logger?.LogInformation("Handling unauthorized Basic auth request.");
 			this.Response.Headers.AppendValues("WWW-Authenticate", $"Basic realm=\"{this.Options.Realm}\"");
 			this.Response.StatusCode = 401; //Unauthorized
 			return Task.FromResult(true);
